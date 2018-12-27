a964a86d6f449f603ff6649bb57a775d61ff1963
Jesse Yowell <jesse.yowell@gmail.com>
2018-12-27 11:57:09 -0800

    assignment 2755: adding album component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
+import Album from './components/Album';
 
 class App extends Component {
   render() {
