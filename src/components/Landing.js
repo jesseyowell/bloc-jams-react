90f95f2b2e1d619ce79170d7238b8dd97977ec7a
Jesse Yowell <jesse.yowell@gmail.com>
2018-12-28 21:28:42 -0800

    Adding landing page information

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,7 +3,22 @@ import React from 'react';
 const Landing = () => (
 
 	<section className="landing">
-		Landing page goes here
+		<h1 className="hero-title">Turn the music up!</h1>
+
+		<section className="selling-points">
+			<div className="point">
+				<h2 className="point-title">Choose your music</h2>
+				<p className="point-description">The world is full of music; why do you have to listen to music somebody else chose?</p>
+			</div>
+			<div className="point">
+				<h2 className="point-title">Unlimited, streaming, ad-free</h2>
+				<p className="point-description">No arbitrary limits, no distractions.</p>
+			</div>
+			<div className="point">
+				<h2 className="point-title">Mobile enabled</h2>
+				<p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+			</div>
+		</section>				
 	</section>	
 );
 
