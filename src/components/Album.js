c76d19567e3c5da13c062fe9e744454a0e885357
Jesse Yowell <jesse.yowell@gmail.com>
2019-01-02 22:39:49 -0800

    Adding play and pause to tracklisting

--- src/components/Album.js
+++ src/components/Album.js
@@ -11,9 +11,39 @@ class Album extends Component {
 		});
 
 		this.state = {
-			album: album
+			album: album,
+			currentSong: album.songs[0],
+			isPlaying: false
 		};
 
+		this.audioElement = document.createElement('audio');
+		this.audioElement.src = album.songs[0].audioSrc;
+	}
+
+	play() {
+		this.audioElement.play();
+		this.setState({ isPlaying: true });
+	}
+
+	pause() {
+		this.audioElement.pause();
+		this.setState({ isPlaying: false });
+	}
+
+	setSong(song) {
+		this.audioElement.src = song.audioSrc;
+		this.setState({ currentSong: song });
+	}
+
+	handleSongClick(song) {
+		const isSameSong = this.state.currentSong === song;
+
+		if(this.state.isPlaying && isSameSong) {
+			this.pause();
+		} else {
+			if(!isSameSong) { this.setSong(song); }
+			this.play();
+		}
 	}
 
 	render() {
@@ -39,7 +69,7 @@ class Album extends Component {
 		   				
 		   				this.state.album.songs.map( (songs,index) => {
 		   					return(
-		   					<tr key={index}>
+		   					<tr className="song" key={index} onClick={ () => this.handleSongClick(songs) }>
 		   						<td>{index+1}.</td>
 		   						<td>{this.state.album.songs[index].title}</td>
 		   						<td>{this.state.album.songs[index].duration} seconds</td>
